f87060598ea861e5cd2c3764090499fc1d684dee
Viktony <nintupple@gmail.com>
2025-01-14 09:45:56 +0100

    Crafting plus hus animationer håber ikke det eksploderer

--- Assets/inside house/player house.cs
+++ Assets/inside house/player house.cs
@@ -11,44 +11,70 @@ public class Player : MonoBehaviour
 
 
     int score;
-    public float Speed = 5;
+    public float speed = 5;
+    private int facingDirection = 1;
+    private float deadzone = 0.1f;
 
+    public Rigidbody2D rb;
+    public Animator anim;
 
-    Rigidbody2D rb;
     Vector3 startPosition;
 
     void Start()
     {
-        startPosition = transform.position;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+   // Update is called once per frame
+    void FixedUpdate()
     {
-        Vector3 direction = new Vector3(0, 0, 0);
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.y = Input.GetAxisRaw("Vertical");
-        direction = Vector3.ClampMagnitude(direction, 1);
-        direction *= Speed;
-        rb.velocity = direction;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
+        if(horizontal > 0 && transform.localScale.x < 0 ||
+             horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        /*         anim.SetFloat("horizontal", Mathf.Abs (horizontal));
+        anim.SetFloat("vertical", vertical); */
+        
+        if(vertical > deadzone && Mathf.Abs (vertical) >= Mathf.Abs (horizontal)){
+            anim.SetBool("walkingUp", true);
+        }
+        else{
+            anim.SetBool("walkingUp", false);
+        }
+        if(vertical < 0f-deadzone && Mathf.Abs (vertical) >= Mathf.Abs (horizontal)){
+            anim.SetBool("walkingDown", true);
+        }
+        else{
+            anim.SetBool("walkingDown", false);
+        }
+        
+        if(Mathf.Abs (horizontal) > deadzone && Mathf.Abs (horizontal) > Mathf.Abs (vertical)){
+            anim.SetBool("walking", true);
+        }
+        else{
+            anim.SetBool("walking", false);
+        }
+        rb.velocity = new Vector2(horizontal, vertical).normalized * speed;
+
+        void Flip()
+        {
+            facingDirection *= -1;
+            transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("door"))
         {
-            SceneManager.LoadScene("Hus Område");
-
+            SceneManager.LoadScene("Hus Omrade");
 
         }
-
-        
-
-
-
-
 
 
         {
